66ecc2c18c66f304c9cf2cfe20c31b176676e815
Xamarin <xamairnguru@gmail.com>
2017-03-21 08:37:12 -0700

    ready to publish iOS to app store

---
+++ LamdanAndroid/Activities/InitActivity.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Widget;
+
+namespace goheja
+{
+	[Activity(Label = "InitActivity", ScreenOrientation = ScreenOrientation.Portrait)]
+	public class InitActivity : BaseActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.InitActivity);
+
+			FindViewById<TextView>(Resource.Id.ActionSignIn).Click += ActionSignIn;
+			FindViewById<TextView>(Resource.Id.ActionSignUp).Click += ActionSignUp;
+		}
+
+		void ActionSignIn(object sender, EventArgs e)
+		{
+			var activity = new Intent(this, typeof(LoginActivity));
+			StartActivityForResult(activity, 1);
+		}
+
+		void ActionSignUp(object sender, EventArgs e)
+		{
+			var activity = new Intent(this, typeof(RegisterActivity));
+			StartActivityForResult(activity, 1);
+		}
+	}
+}
