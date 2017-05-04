6dc14640e799e468ce22923a4b1f70337c3b477c
Xamarin <xamairnguru@gmail.com>
2017-05-04 10:27:45 -0700

    check again

--- LamdanAndroid/Activities/CoachHomeActivity.cs
+++ LamdanAndroid/Activities/CoachHomeActivity.cs
@@ -25,9 +25,7 @@ namespace goheja
 
             SetContentView(Resource.Layout.CoachHomeActivity);
 
-
 			var config = ImageLoaderConfiguration.CreateDefault(ApplicationContext);
-			// Initialize ImageLoader with configuration.
 			ImageLoader.Instance.Init(config);
 
 			InitUISettings();
